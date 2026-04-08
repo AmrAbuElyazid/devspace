612405f11786898c9a1a4aea05b47e205fb5c41f
AmrAbuElyazid <aboelyazid.amr@gmail.com>
2026-04-08 21:53:36 +0200

    refactor: split renderer stylesheet sections

--- apps/desktop/src/renderer/main.tsx
+++ apps/desktop/src/renderer/main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
+import "./styles/workspace-shell.css";
+import "./styles/browser-pane.css";
+import "./styles/ui-primitives.css";
 import {
   initializeWorkspaceStore,
   resetWorkspaceStoreToDefaults,
